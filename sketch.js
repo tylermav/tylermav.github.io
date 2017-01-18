05ee2a6db01833dee800576df09dbd36e791cb10
Tyler Ma <tylermav@gmail.com>
2017-01-18 15:06:30 -0600

    blah

--- sketch.js
+++ sketch.js
@@ -1,23 +1,27 @@
-var angle = 0;
+var angleX = 0;
+var angleY = 0;
+var twoD1 = new createCanvas(windowWidth,windowHeight);
+var twoD2 = new createCanvas(windowWidth,windowHeight);
 
 function setup() {
+//YOU NEED TO WORK ON GETTING THE LOCATION OF THE CANVAS ON TOP OF THE WEBGL	
   createCanvas(windowWidth, windowHeight, WEBGL);
 
+}
+function mouseDragged() {
+	angleX += mouseX/90000;
+	angleY += mouseY/90000;
 }
 
 function draw() {
   background(51);
-
   rotateX(angleX);
   rotateY(angleY);
+
+  //line((windowWidth*(9/10)),(windowHeight*(1/8),(windowWidth*(1/10)),(windowHeight*(7/8)));
   box(200);
 }
 
-function mouseDragged() {
-	angleX += mouseX;
-	angleY += mouseY;
-
-}
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
